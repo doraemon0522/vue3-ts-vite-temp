292f676561fb44b9d836a3f88951e8032303045e
zhengying <ying.zheng@di-matrix.com>
2025-03-28 16:45:38 +0800

    feat: 三维地图 demo 研究

--- src/components/lmBall/terrain3DSimple.vue
+++ src/components/lmBall/terrain3DSimple.vue
@@ -1,22 +1,37 @@
 <template>
-  <div
-    id="cesiumContainer"
-    ref="containerRef"
-    style="height: 500px; width: 500px; position: relative"
-  ></div>
+  <div id="cesiumContainer" ref="containerRef" style="height: 500px; width: 500px"></div>
 </template>
 <script setup lang="ts">
   import { onMounted, ref } from 'vue';
   import * as Cesium from 'cesium';
   import * as THREE from 'three';
-  import { IonResource, Cesium3DTileset } from 'cesium';
+  /**
+   * @description: 3D球体 调试成功版
+   */
 
   const containerRef = ref(null);
+  const viewer = ref(null);
+  const addThreeJSModel = (viewer) => {
+    const scene = viewer.scene;
+    const threeScene = new THREE.Scene();
+
+    const geometry = new THREE.SphereGeometry(500, 32, 32);
+    const material = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+    const sphere = new THREE.Mesh(geometry, material);
+    threeScene.add(sphere);
+
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    threeScene.add(ambientLight);
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(1, 1, 1);
+    threeScene.add(directionalLight);
+  };
 
   onMounted(async () => {
     if (!containerRef.value) return;
     Cesium.Ion.defaultAccessToken =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmZjZjOTU2YS04ODVhLTQyMWYtOTc2OS01MTcwMzc4OWNmNjMiLCJpZCI6MjgyMTUyLCJpYXQiOjE3NDEzMzkwNTh9.dWV-wY3kk7dFEIJsnZB6EnQk_3va49uplmW9DRMSKfo';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiMDM0MzE0ZS0xODkxLTRiYTYtODg0ZC1hZGEwNDJhMmE4ZTIiLCJpZCI6MjgyMTUyLCJpYXQiOjE3NDEzMzY3Nzd9.Tc5b_Aq7-wo_oOskCKgTmQqXzu4Qtg5fYxzQdesiG4Q';
 
     const viewer = new Cesium.Viewer('cesiumContainer', {
       shouldAnimate: true,
@@ -34,21 +49,11 @@
       navigationInstructionsInitiallyVisible: false, // 关闭导航说明
     });
     viewer.cesiumWidget.creditContainer.style.display = 'none';
-
-    try {
-      const google3DTilesAssetId = 2275207;
-      const tileset = await Cesium3DTileset.fromIonAssetId(google3DTilesAssetId);
-      viewer.scene.primitives.add(tileset);
-      console.log('✅ 成功加载 Google 3D Tiles');
-    } catch (err) {
-      console.log(err);
-    }
-
     viewer.camera.flyTo({
-      destination: Cesium.Cartesian3.fromDegrees(120.23, 30.205, 1000), // 杭州坐标
+      destination: Cesium.Cartesian3.fromDegrees(120.1551, 30.2741, 3000), // 杭州坐标
       orientation: {
         heading: Cesium.Math.toRadians(0), // 朝北
-        pitch: Cesium.Math.toRadians(-45), // 俯视角度
+        pitch: Cesium.Math.toRadians(-30), // 俯视角度
         roll: 0,
       },
       duration: 1, // 飞行时间 3 秒
